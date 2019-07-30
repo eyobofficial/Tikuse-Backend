a9dd93cfe93097c090e8785c24d0cd7ced8e95ae
eyob tariku <eyobtariku@gmail.com>
2019-07-30 05:55:43 +0300

    [#37] - Create a `CustomUser` and `Profile` models

--- accounts/admin.py
+++ accounts/admin.py
@@ -6,18 +6,22 @@ from .models import CustomUser, Profile
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
     list_display = (
+        'public_id',
         'username',
+        'phone_number',
         'email',
         'is_active',
         'is_staff',
         'is_superuser',
         'last_login'
     )
-    list_display_link = ('username', 'email')
+    list_display_link = ('username', 'phone_number')
     list_filter = ('is_active', 'is_staff', 'is_superuser')
-    search_fields = ('username', 'email', 'first_name', 'last_name')
+    search_fields = ('username', 'email', 'phone_number')
 
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', )
+    list_display = ('user', 'full_name')
+    list_display_link = ('user', 'full_name')
+    search_fields = ('full_name', )
--- accounts/models.py
+++ accounts/models.py
@@ -1,15 +1,46 @@
 from uuid import uuid4
+
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from phonenumber_field.modelfields import PhoneNumberField
+
+from shared.utilities.commons import generate_public_id
+
 
 class CustomUser(AbstractUser):
     id = models.UUIDField(primary_key=True, editable=False, default=uuid4)
+    public_id = models.CharField(
+        max_length=12,
+        unique=True,
+        editable=False,
+        default=generate_public_id
+    )
+    phone_number = PhoneNumberField(unique=True)
+
+    def __str__(self):
+        return self.username
 
 
 class Profile(models.Model):
+
+    # User types
+    HOST = 'HOST'
+    GUEST = 'GUEST'
+
+    TYPE_CHOICES = (
+        (HOST, 'Host'),
+        (GUEST, 'Guest')
+    )
+
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE
     )
+    full_name = models.CharField(max_length=120)
+    type = models.CharField(max_length=5, choices=TYPE_CHOICES)
+    is_activated = models.BooleanField('activated', default=False)
+
+    def __str__(self):
+        return self.full_name
