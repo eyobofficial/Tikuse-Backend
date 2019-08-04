90f5e29cd7a3ea503ac1675b8ffd186dfaa5f7bf
eyob tariku <eyobtariku@gmail.com>
2019-08-04 13:22:04 +0300

    [#7] - Remove `/api/` from base URLs

--- config/urls.py
+++ config/urls.py
@@ -36,11 +36,11 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(
-        'api/v1/accounts/',
+        'v1/accounts/',
         include('accounts.urls', namespace='v1-accounts')
     ),
     path(
-        'api/v1/',
+        'v1/',
         schema_view.with_ui('swagger', cache_timeout=0),
         name='docs'
     ),
