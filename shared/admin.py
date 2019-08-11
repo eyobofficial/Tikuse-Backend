f44142469bc1d9f9b10ccf3758e5b01e374aaec4
eyob tariku <eyobtariku@gmail.com>
2019-08-11 12:48:18 +0300

    [#21] - Add an `SMS` model to `shared` app
    After adding the model, create the necesssary migration files and
    register the model the admin site.

--- shared/admin.py
+++ shared/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from django.conf.urls import url
 
-# Register your models here.
+from .models import SMS
+
+
+class CustomURLModelAdmin(admin.ModelAdmin):
+    def get_urls(self):
+        urls = super().get_urls()
+        additional_urls = []
+
+        for custom_url in self.custom_urls:
+            view = custom_url['view'].as_view()
+            custom_url['view'] = self.admin_site.admin_view(view)
+            additional_urls.append(url(**custom_url))
+
+        return additional_urls + urls
+
+
+@admin.register(SMS)
+class SMSMessageAdmin(admin.ModelAdmin):
+    list_display = (
+        'subject',
+        'recipient',
+        'type',
+        'provider',
+        'sent_at'
+    )
+    readonly_fields = (
+        'subject',
+        'provider',
+        'type',
+        'sender',
+        'recipient',
+        'message',
+        'sent_at'
+    )
+    list_filter = ('type', 'provider')
+    search_fields = ('subject', 'message')
--- shared/models.py
+++ shared/models.py
@@ -1,3 +1,54 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class SMS(models.Model):
+    # SMS Providers
+    AFRICASTALKING = 'AFRICAS_TALKING'
+    TWILIO = 'TWILIO'
+
+    provider_choices = (
+        (AFRICASTALKING, 'Africas Talking'),
+        (TWILIO, 'Twilio')
+    )
+
+    # SMS message type
+    NOTIFICATION = 'NOTIFICATION'
+    REMINDER = 'REMINDER'
+    ADVERTISEMENT = 'ADVERTISEMENT'
+
+    type_choices = (
+        (NOTIFICATION, 'Notification'),
+        (REMINDER, 'Reminder'),
+        (ADVERTISEMENT, 'Advertisement')
+    )
+
+    subject = models.CharField(max_length=120)
+    provider = models.CharField(
+        max_length=30,
+        choices=provider_choices,
+        default=AFRICASTALKING
+    )
+    type = models.CharField(
+        max_length=30,
+        choices=type_choices,
+        default=NOTIFICATION
+    )
+    sender = models.CharField(max_length=30, blank=True)
+    recipient = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='messages'
+    )
+    message = models.TextField()
+    sent_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-sent_at', )
+        verbose_name = 'SMS'
+        verbose_name_plural = 'SMS'
+
+    def __str__(self):
+        return f'{self.subject} - <{self.recipient.full_name}>'
